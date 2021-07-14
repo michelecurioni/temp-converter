ece687d0845f6c926727e7ffb4c6e4c99c173c02
michele.curioni <michele@fujitsu>
2021-07-14 09:03:12 +0100

    added first conversion function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,8 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   @Input() firstValue: number = 0;
   secondValue = 0
+  selectedUnitFrom: string = 'celsius';
+  selectedUnitTo: string = 'fahrenheit';
 
   constructor() { }
 
@@ -18,7 +20,46 @@ export class HomeComponent implements OnInit {
   }
 
   convert(){
+    let tempValue:number = 0;
+    switch(this.selectedUnitTo) {
+       case 'fahrenheit': {
+         switch(this.selectedUnitFrom) {
+            case 'celsius': {
+               this.secondValue = this.fromCelsiusToFahrenheit(this.firstValue);
+               break;
+            }
+          }
+          break;
+       }
+       default: {
+          //statements;
+          break;
+       }
+    }
     console.log("First Value = " + this.firstValue);
   }
+
+
+
+  //event handler for the select element's change event
+  unitFromChangeHandler (event: any) {
+    //update the ui
+    this.selectedUnitFrom = event.target.value;
+    console.log("Selected From = " + this.selectedUnitFrom);
+  }
+
+  //event handler for the select element's change event
+  unitToChangeHandler (event: any) {
+    //update the ui
+    this.selectedUnitTo = event.target.value;
+    console.log("Selected To = " + this.selectedUnitFrom);
+  }
+
+  fromCelsiusToFahrenheit(value : number) : number {
+    let formula:string = "(°C × 9/5) + 32 = °F"
+
+    return (value * 9/5) + 32;
+  }
+
 
 }
